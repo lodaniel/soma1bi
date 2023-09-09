79291e225c123443f7878a9bd9c629bfac05662a
Luciano Daniel <42324730+lodaniel@users.noreply.github.com>
2023-09-08 23:48:49 -0300

    Add files via upload
    Inclusão de comentario sobre compilação com flags de otimização.

---
+++ soma2bi.c
@@ -0,0 +1,19 @@
+// Para gerar executavel otimizado usar "gcc -O3 -march=native -o soma2bi.exe soma2bi.c"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    long long tot = 0;
+    long long n = 1000000000;
+    clock_t ti = clock();
+    for (long long i = 1; i <= n; i++) {
+        tot += i;
+    }
+    clock_t tf = clock();
+    double dt = (double)(tf-ti)/CLOCKS_PER_SEC;
+    printf("Soma: %lld\n", tot);
+    printf("Tempo: %.15lf\n", dt);
+    return 0;
+}
